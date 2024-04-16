cd8b9d28a3c901b55d1902551b665f6d14383534
Simon Lund <simon97lund@gmail.com>
2024-04-16 23:46:45 +0200

    :recycle: rework server
    - add cors middleware
    - rework values of enums
    - rename enums and adapt parameter names
    - move parameters into request body (multipart/form-data)

--- src/edutap/image_api/main.py
+++ src/edutap/image_api/main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
-from enum import StrEnum
-from fastapi import FastAPI
+from enum import StrEnum, auto
+from fastapi import FastAPI, Form, File
 from fastapi import UploadFile
 from fastapi.logger import logger
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from importlib.metadata import version
 from PIL import Image
 from PIL import ImageDraw
@@ -42,6 +43,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
@@ -56,66 +69,80 @@ async def openapi():
     return app.openapi()
 
 
-class MaskFormats(StrEnum):
-    NONE = "No Mask"
-    CIRCLE = "Circle Mask"
-    BOX = "Box Mask"
+class MaskTypeEnum(StrEnum):
+    NONE = auto()
+    """No Mask"""
+    CIRCLE = auto()
+    """Circle Mask"""
+    BOX = auto()
+    """Box Mask"""
 
 
-class AspectRatios(StrEnum):
-    FREE = "Free (no predefined Aspect Ratio, please define width and hight manually)"
-    SQUARE = "Square (Aspect Ratio 1:1)"
-    LANDSCAPE_3x2 = "Landscape (Aspect Ratio 3:2 (Width:Height))"
-    LANDSCAPE_4x3 = "Landscape (Aspect Ratio 4:3 (Width:Height))"
-    LANDSCAPE_16x9 = "Landscape (Aspect Ratio 16:9 (Width:Height))"
-    LANDSCAPE_16x10 = "Landscape (Aspect Ratio 16:10 (Width:Height))"
-    PORTRAIT_3x4 = "Portrait (Aspect Ratio 3:4 (Width:Height))"
+class AspectRatioEnum(StrEnum):
+    SQUARE = auto()
+    """Square (Aspect Ratio 1:1)"""
+    LANDSCAPE_3x2 = auto()
+    """Landscape (Aspect Ratio 3:2 (Width:Height))"""
+    LANDSCAPE_4x3 = auto()
+    """Landscape (Aspect Ratio 4:3 (Width:Height))"""
+    LANDSCAPE_16x9 = auto()
+    """Landscape (Aspect Ratio 16:9 (Width:Height))"""
+    LANDSCAPE_16x10 = auto()
+    """Landscape (Aspect Ratio 16:10 (Width:Height))"""
+    PORTRAIT_3x4 = auto()
+    """Portrait (Aspect Ratio 3:4 (Width:Height))"""
+    FREE = auto()
+    """No predefined Aspect Ratio, width and height must be defined manually)"""
 
 
 @app.post("/crop/", response_class=FileResponse)
 async def crop_file(
-    file: UploadFile,
-    *,
-    mask: Annotated[Literal[MaskFormats.NONE, MaskFormats.CIRCLE, MaskFormats.BOX], MaskFormats] = MaskFormats.NONE,
-    image_size: Annotated[Literal[AspectRatios.FREE, AspectRatios.SQUARE, AspectRatios.PORTRAIT_3x4], AspectRatios] = AspectRatios.SQUARE,
-    height: Annotated[int, "Hight of result Image"] = 1000,
-    width: Annotated[int | Literal["auto", "AUTO"], "Width of result Image"] = "auto",
-    radius: Annotated[int, "Radius of Mask Box, if mask == BOX"] = 100,
+    file: Annotated[UploadFile, File(description="Image File")],
+    mask: Annotated[MaskTypeEnum, Form(description="Mask Type")] = MaskTypeEnum.NONE,
+    aspect_ratio: Annotated[AspectRatioEnum, Form(description="Aspect Ratio of result Image")] = AspectRatioEnum.SQUARE,
+    height: Annotated[int, Form(description="Height of result Image")] = 1000,
+    width: Annotated[int | Literal["auto"], Form(description="Width of result Image")] = "auto",
+    radius: Annotated[int, Form(description="Radius of Mask Box, if mask == BOX")] = 100,
 ):
-    logger.debug(file.filename)
-    logger.debug(file.size)
-    logger.debug(file.headers)
-    logger.debug(mask)
+    logger.debug(f"Filename: {file.filename}")
+    logger.debug(f"Filesize: {file.size}")
+    logger.debug(f"File Headers: {file.headers}")
+    logger.debug(f"Mask: {mask}")
+    logger.debug(f"Aspect Ratio: {aspect_ratio}")
+    logger.debug(f"Height: {height}")
+    logger.debug(f"Width: {width}")
+    logger.debug(f"Radius: {radius}")
 
+    # Note: When uncommenting, remove the response_class=FileResponse from the function signature, otherwise this will raise an error
     # return {"file-name": file.filename, "file-size": file.size}
 
     photo = Image.open(file.file)
 
-    if width in ["auto", "AUTO"] and image_size == AspectRatios.FREE:
+    if width in ["auto", "AUTO"] and aspect_ratio == AspectRatioEnum.FREE:
         raise ValueError(
-            "Key width-value is 'auto', which is not allowed for image_size: free"
+            "Key width-value is 'auto', which is not allowed for aspect_ratio: free"
         )
     elif width in ["auto", "AUTO"]:
-        if image_size == AspectRatios.SQUARE:
+        if aspect_ratio == AspectRatioEnum.SQUARE:
             width = height
-        elif image_size == AspectRatios.PORTRAIT_3x4:
+        elif aspect_ratio == AspectRatioEnum.PORTRAIT_3x4:
             width = int(height / 4 * 3)
 
     else:
-        # case where width is an actual int value and image_size is free
+        # case where width is an actual int value and aspect_ratio is free
         pass
     assert isinstance(width, int)
 
     photo = photo.resize((width, height))
     mask_image = Image.new("RGBA", size=(width, height))
 
-    if mask == MaskFormats.CIRCLE:
+    if mask == MaskTypeEnum.CIRCLE:
         mask_circle = ImageDraw.Draw(mask_image)
         mask_circle.ellipse((0, 0, width, height), fill="#ffffff")
-    elif mask == MaskFormats.BOX:
+    elif mask == MaskTypeEnum.BOX:
         mask_box = ImageDraw.Draw(mask_image)
         mask_box.rounded_rectangle((0, 0, width, height), radius=radius, fill="#ffffff")
-    elif mask == MaskFormats.NONE:  # Default Case
+    elif mask == MaskTypeEnum.NONE:  # Default Case
         pass
     else:
         raise KeyError(f"Unknown Mask Type: {mask}")
@@ -123,7 +150,7 @@ async def crop_file(
     output_file = tempfile.mkstemp(suffix=".png")[1]
     # breakpoint()
 
-    if mask in (MaskFormats.CIRCLE, MaskFormats.BOX):
+    if mask in (MaskTypeEnum.CIRCLE, MaskTypeEnum.BOX):
         background = Image.new("RGBA", size=(width, height))
         result_image = Image.composite(photo, background, mask_image)
         result_image.save(output_file, format="PNG", optimize=True)
